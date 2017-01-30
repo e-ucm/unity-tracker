6351361acd85d0e756ea4addd92f11793a03baa4
Victormafire <victorma-fire@hotmail.com>
2017-01-30 11:45:19 +0100

    Namespaces
    + Added namespace Rage.Analytics for the tracker
    + Added namespace Rage.Analytics.Formats for the formats
    + Added namespace Rage.Analytics.Storages for the storages

--- Tracker/Storage/LocalStorage.cs
+++ Tracker/Storage/LocalStorage.cs
@@ -20,88 +20,90 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-
-public class LocalStorage : Storage
+namespace RAGE.Analytics.Storages
 {
-	private const string Separator = "--session--,";
-	private string tracesPathPrefix;
-	private string tracesFile;
-		
-	public LocalStorage (string tracesPathPrefix)
+	public class LocalStorage : Storage
 	{
-		this.tracesPathPrefix = tracesPathPrefix;
-	}
-				
-	public void SetTracker (Tracker tracker)
-	{
-	}
-		
-	public void Start (Net.IRequestListener startListener)
-	{
-		string now = System.DateTime.Now.ToString ().Replace('/', '_').Replace(':', '_');
-		tracesFile = tracesPathPrefix + ".csv";	
-		Write("\n" + Separator + now + "\n", startListener);
-	}
-
-	public void Send (String data, Net.IRequestListener flushListener)
-	{
-		Write (data, flushListener);
-	}
-
-	private void Write (String data, Net.IRequestListener requestListener)
-	{
-
-#if UNITY_WEBGL
-		requestListener.Error ("Impossible to use LocalStorage in WebGL version");
-#elif UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX
-		try {
-			File.AppendAllText (tracesFile, data);
-			requestListener.Result ("");
-		} catch (Exception e) {
-			requestListener.Error (e.Message);
-		}	
-#endif
-	}
-
-	public bool IsAvailable ()
-	{
-#if UNITY_WEBGL
-     return false;
-#else
-		return true;
-#endif
-	}
-
-	public void CleanFile ()
-	{
-#if !(UNITY_WEBPLAYER || UNITY_WEBGL)
-		if (File.Exists (tracesFile))
+		private const string Separator = "--session--,";
+		private string tracesPathPrefix;
+		private string tracesFile;
+			
+		public LocalStorage (string tracesPathPrefix)
 		{
-			File.WriteAllText (tracesFile, "");
+			this.tracesPathPrefix = tracesPathPrefix;
 		}
-#endif
-    }
-
-    public List<string> RecoverData ()
-	{
-		List<String> tracesList = new List<String>();
-#if UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX
-		if (File.Exists(tracesFile))
+					
+		public void SetTracker (Tracker tracker)
 		{
-			string file = tracesFile;
-			string data = File.ReadAllText(file);
-			String[] traces = data.Split(new Char[] { ';', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+		}
+			
+		public void Start (Net.IRequestListener startListener)
+		{
+			string now = System.DateTime.Now.ToString ().Replace('/', '_').Replace(':', '_');
+			tracesFile = tracesPathPrefix + ".csv";	
+			Write("\n" + Separator + now + "\n", startListener);
+		}
 
-			foreach (String s in traces)
+		public void Send (String data, Net.IRequestListener flushListener)
+		{
+			Write (data, flushListener);
+		}
+
+		private void Write (String data, Net.IRequestListener requestListener)
+		{
+
+	#if UNITY_WEBGL
+			requestListener.Error ("Impossible to use LocalStorage in WebGL version");
+	#elif UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX
+			try {
+				File.AppendAllText (tracesFile, data);
+				requestListener.Result ("");
+			} catch (Exception e) {
+				requestListener.Error (e.Message);
+			}	
+	#endif
+		}
+
+		public bool IsAvailable ()
+		{
+	#if UNITY_WEBGL
+		return false;
+	#else
+			return true;
+	#endif
+		}
+
+		public void CleanFile ()
+		{
+	#if !(UNITY_WEBPLAYER || UNITY_WEBGL)
+			if (File.Exists (tracesFile))
 			{
-				if (!String.IsNullOrEmpty(s) && s.Substring(0, Separator.Length) != Separator)
+				File.WriteAllText (tracesFile, "");
+			}
+	#endif
+		}
+
+		public List<string> RecoverData ()
+		{
+			List<String> tracesList = new List<String>();
+	#if UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX
+			if (File.Exists(tracesFile))
+			{
+				string file = tracesFile;
+				string data = File.ReadAllText(file);
+				String[] traces = data.Split(new Char[] { ';', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+				foreach (String s in traces)
 				{
-					tracesList.Add(s);
+					if (!String.IsNullOrEmpty(s) && s.Substring(0, Separator.Length) != Separator)
+					{
+						tracesList.Add(s);
+					}
 				}
 			}
+	#endif
+			return tracesList;
 		}
-#endif
-		return tracesList;
 	}
 }
 
--- Tracker/Storage/Storage.cs
+++ Tracker/Storage/Storage.cs
@@ -17,19 +17,22 @@
  */
 using System;
 
-public interface Storage
+namespace RAGE.Analytics.Storages
 {
-	void SetTracker (Tracker tracker);
-	
-	/// <summary>
-	/// The tracker wants to start sending traces
-	///</summary>
-	void Start (Net.IRequestListener startListener);
-	
-	///<summary>
-	/// The tracker wants to send the given data
-	///</summary>
-	void Send (String data, Net.IRequestListener flushListener);
+	public interface Storage
+	{
+		void SetTracker (Tracker tracker);
+		
+		/// <summary>
+		/// The tracker wants to start sending traces
+		///</summary>
+		void Start (Net.IRequestListener startListener);
+		
+		///<summary>
+		/// The tracker wants to send the given data
+		///</summary>
+		void Send (String data, Net.IRequestListener flushListener);
 
-	bool IsAvailable();
+		bool IsAvailable();
+	}
 }
