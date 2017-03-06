217dd628f906cc861d730f8a83c642860c5fd631
Iván J. Pérez <synpheros@hotmail.com>
2017-03-06 18:53:30 +0100

    Added more Exceptions

--- Tracker/Exception/XApiException.cs
+++ Tracker/Exception/XApiException.cs
@@ -4,15 +4,8 @@ using System;
 namespace RAGE.Analytics.Exceptions{
 
     public class XApiException : TrackerException{
-        public enum XApiExceptionType { VERB, ACTOR, TARGET, EXTENSION };
 
-        public XApiExceptionType Type {
-            get;
-            private set;
-        }
-
-        public XApiException(string message, XApiExceptionType type) : base(message){
-            this.Type = type;
+        public XApiException(string message) : base(message){
         }
     }
 }
